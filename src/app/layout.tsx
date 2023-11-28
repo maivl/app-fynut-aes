a72ac4baa707a50e928099b6f9c45d304b6aa3ac
maivl <wkf39988@gmail.com>
2023-11-28 04:22:54 +0000

    feat: update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import StyledComponentsRegistry from './lib/AntdRegistry';
+import StyledComponentsRegistry from '../lib/AntdRegistry';
 import './globals.scss';
 
 const inter = Inter({ subsets: ['latin'] });
