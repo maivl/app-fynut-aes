7b9561bd71d7654a012d88ff118ddfb5c2df21c3
maivl <wkf39988@gmail.com>
2023-11-28 04:32:45 +0000

    feat: update

--- src/lib/AntdRegistry.tsx
+++ src/lib/AntdRegistry.tsx
@@ -6,17 +6,17 @@ import type Entity from '@ant-design/cssinjs/es/Cache';
 import { useServerInsertedHTML } from 'next/navigation';
 
 const StyledComponentsRegistry = ({ children }: React.PropsWithChildren) => {
-  const cache = React.useMemo<Entity>(() => createCache(), []);
-  const isServerInserted = React.useRef<boolean>(false);
-  useServerInsertedHTML(() => {
+    const cache = React.useMemo<Entity>(() => createCache(), []);
+    const isServerInserted = React.useRef<boolean>(false);
+    useServerInsertedHTML(() => {
     // avoid duplicate css insert
-    if (isServerInserted.current) {
-      return;
-    }
-    isServerInserted.current = true;
-    return <style id="antd" dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }} />;
-  });
-  return <StyleProvider cache={cache}>{children}</StyleProvider>;
+        if (isServerInserted.current) {
+            return;
+        }
+        isServerInserted.current = true;
+        return <style id="antd" dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }} />;
+    });
+    return <StyleProvider cache={cache}>{children}</StyleProvider>;
 };
 
 export default StyledComponentsRegistry;
